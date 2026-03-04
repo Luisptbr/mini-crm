4fe59858af7ecee887d010093e0c401522b7e4f7
Luisptbr <luis_henrique2411@hotmail.com>
2026-03-04 00:06:46 -0300

    feat: adiciona camada de tratamento de exceções

---
+++ backend-crm/src/main/java/com/luis/crm/marcenaria/exception/UsuarioException.java
@@ -0,0 +1,9 @@
+package com.luis.crm.marcenaria.exception;
+
+public class UsuarioException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public UsuarioException(String mensagem) {
+		super(mensagem);
+	}
+
+}
